d5ce3aca93af1a51735d004763ceb7170bdf1ffc
Isidro Aguirre <ausi280@gmail.com>
2026-03-21 19:21:19 -0600

    oauth changes

--- api/src/controllers/erpController.js
+++ api/src/controllers/erpController.js
@@ -14,23 +14,6 @@ class ErpController {
 
     this.service = env.SERVICES.ERP;
 
-
-    this.token = {
-      key: this.service.ACCESS_TOKEN,
-      secret: this.service.TOKEN_SECRET,
-    };
-
-    this.oauth = OAuth({
-      consumer: {
-        key: this.service.CONSUMER_KEY,
-        secret: this.service.CONSUMER_SECRET,
-      },
-      signature_method: 'HMAC-SHA256',
-      hash_function(base, key) {
-        return crypto.createHmac('sha256', key).update(base).digest('base64');
-      },
-    });
-
     this.api = axios.create({
       baseURL: this.service.URL,
       headers: { 
@@ -38,60 +21,6 @@ class ErpController {
       },
     });
 
-  }
-
-  #send = async (method, params, data ) => {
-
-    const request = {
-      url: this.service.URL,
-      method: method,
-      headers: {},
-      data : params
-    }
-
-    request.headers = this.oauth.toHeader(
-        this.oauth.authorize(request, this.token)
-    );
-
-    request.headers.Authorization = `OAuth realm="${this.service.REALM}", ${request.headers.Authorization.replace('OAuth ', '')}`;
-
-    const response = await axios({
-      method,
-      url: request.url,
-      params: params,
-      data: data,
-      headers: {
-        'Content-Type': 'application/json',
-        ...request.headers
-      }
-    });
-
-    return response;
-  }
-
-  #process = async (res, callback, options = {}) => {
-
-    const { returnData = false } = options;
-
-    try {
-
-      const response = await callback();
-
-      if( returnData )
-        return response.data;
-
-      res.status(response.status).json(response.data);
-
-    } catch (error) {
-
-      if( returnData )
-        throw error;
-
-      const status = error.response?.status || 500;
-      const data = error.response?.data || { error: error.message };
-      res.status(status).json({data: data});
-
-    }
   }
 
   syncNetsuiteData = async (req, res) => {
