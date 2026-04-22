56263edb016ce33a685884e0ec7f6e7c54c2fc8b
Isidro Aguirre <ausi280@gmail.com>
2026-04-21 19:19:27 -0600

    webhook v1

---
+++ api/src/routes/metaRoutes.js
@@ -0,0 +1,7 @@
+/**
+ * This file exports the router defined in the metaWebhooks controller.
+ * server.js will automatically mount this at the SLUG defined in config.
+ */
+const metaWebhookRouter = require('../controllers/metaWebhooks');
+
+module.exports = metaWebhookRouter;module.exports = metaWebhookRouter;
